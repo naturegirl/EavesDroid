de07280b87b4aab9ee02cc73145bbe9c92b93487
Stephen Lin <yihsien@princeton.edu>
2014-04-24 21:48:31 -0400

    mclust file

---
+++ signal-plotting/R/signal_cluster.R
@@ -0,0 +1,23 @@
+library(mclust)
+library(scatterplot3d)
+
+path <- "~/Documents/School/Interacting_With_Data/cos424project/signal-plotting/data/nm.features.csv"
+
+signals <- read.csv(path, header=TRUE, sep=",")
+
+colum <- ncol(signals)
+row <- nrow(signals)
+
+#print(signals)
+
+result<-Mclust(signals[,1:5], G=3)
+print(summary(result))
+print(result)
+plot(result, what="classification")
+par(new=TRUE)
+mtext("",side=2, line=0)
+cluster_mapping <- cbind(signals, result$classification)
+print(cluster_mapping)
+
+
+
