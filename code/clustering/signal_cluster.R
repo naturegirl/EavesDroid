40605a720e81bda44b555f1f09b6954991177c5e
Stephen Lin <yihsien@princeton.edu>
2014-05-03 18:02:08 -0400

    modified cluster.r

--- code/clustering/signal_cluster.R
+++ code/clustering/signal_cluster.R
@@ -4,10 +4,10 @@ library(rpart)
 library(scatterplot3d)
 
 #path <- "~/Documents/School/Interacting_With_Data/cos424project/signal-plotting/data/nm.features.csv"
-path <- "~/Documents/School/Interacting_With_Data/cos424project/data/features/lr-all-combined.csv"
+path <- "~/Documents/School/Interacting_With_Data/cos424project/data/features/data-a-z_combined.csv"
 #path <- "~/Documents/School/Interacting_With_Data/cos424project/data/LR_output/lr-labelled-training-data.csv"
 
-signals <- read.csv(path, header=TRUE, sep=",")
+signals <- read.csv(path, header=TRUE, sep=",", stringsAsFactors = FALSE)
 signals <- data.matrix(signals)
 cols <- ncol(signals)
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
@@ -18,11 +18,11 @@ signals <- cbind(range01(signals[,-cols]), label=signals[,cols])
 features <- signals[,1:cols-1]
 label <- signals[,cols]
 
-result<-Mclust(features, G=5)
+result<-Mclust(features, G=26)
 #print(summary(result))
 #plot(result, what="classification")
 cluster_mapping <- cbind(label, result$classification)
-#print(cluster_mapping)
+print(cluster_mapping)
 #print(result$classification)
 row <- nrow(cluster_mapping)
 clusterLetter <- array(0, dim=c(26,26))
@@ -32,11 +32,11 @@ for(r in 1:row){
     clusterLetter[cluster_mapping[r,1], cluster_mapping[r,2]] + 1
 
 }
-#print(clusterLetter)
+print(clusterLetter)
 
-clusterLabeling <- array(0, dim=c(1,26))
+clusterLabeling <- array(0, dim=c(1, 26))
 for(c in 1:col){
-  clusterLabeling[1,c] = which.max(clusterLetter[,c])
+  clusterLabeling[1,c] = which.max(clusterLetter[, c])
 }
 
 print(clusterLabeling)
@@ -48,7 +48,7 @@ testset <- signals[testindex,]
 trainset <- signals[-testindex,]
 
 #svm
-svm.model <- svm( as.factor(label) ~ . , data = trainset, cost = 120, gamma = 1)
+svm.model <- svm( as.factor(label) ~ . , data = trainset, cost = 100, gamma = 1)
 svm.pred <- predict(svm.model, testset[,-cols])
 
 ## compute svm confusion matrix
