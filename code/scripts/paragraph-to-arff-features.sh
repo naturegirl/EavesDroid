08bfb9d076bd0779b31274563bc463e12dac5175
Akshay Mittal <akshay@cs.princeton.edu>
2014-05-11 01:43:06 -0400

    training: letters to features extractor $ training-feature-extractor.sh all-letters $ combine_features.sh all-letters

--- code/scripts/paragraph-to-arff-features.sh
+++ code/scripts/paragraph-to-arff-features.sh
@@ -13,17 +13,17 @@ fi
 path=../../data
 
 # create copy of all the .csv files into a new dir
-rm -rf $path/__data
-mkdir $path/__data
-mkdir $path/__data/files
-cp -r $path/$1/* $path/__data/files
-printf "copy: all *.csv copied to $path/__data\n\n"
+rm -rf $path/__para
+mkdir $path/__para
+mkdir $path/__para/files
+cp -r $path/$1/* $path/__para/files
+printf "copy: all *.csv copied to $path/__para\n\n"
 
 # run the start/stop ends removing-script on the dataset
-python ../preprocessor/ends_clipper.py $path/__data $path/__data/clipped
-rm -rf $path/__data/files
-mv $path/__data/clipped/* $path/__data
-rm -rf $path/__data/clipped
+python ../preprocessor/ends_clipper.py $path/__para $path/__para/clipped
+rm -rf $path/__para/files
+mv $path/__para/clipped/* $path/__para
+rm -rf $path/__para/clipped
 printf "clipper: start/stop ends clipped\n\n"
 
 # copy the class files to this directory
@@ -32,26 +32,28 @@ mv ../feature_extractor/*.class .
 echo "classes: files moved to this directory\n\n"
 
 # generate the individual letter files
-java -cp . WordFeatureExtractor __data timexyz
-mv $path/__data.indivletters $path/__data
-mv $path/__data.letters.gforces $path/__data
+java -cp . WordFeatureExtractor __para timexyz
+mv $path/__para.indivletters $path/__para
+mv $path/__para.letters.gforces $path/__para
 printf "individual: letters files generated\n\n"
 
 # label the individual letter files
-mkdir $path/__data/labels
-cp ../labeler/data/* $path/__data/labels/
-cd $path/__data/__data.indivletters
+mkdir $path/__para/labels
+cp ../labeler/data/* $path/__para/labels/
+cd $path/__para/__para.indivletters
 numdirs=$(ls -l -d */ | wc -l)
 dir=1
-rm -rf ../__data.labeled.letters
-mkdir ../__data.labeled.letters
+rm -rf ../__para.labeled.letters
+mkdir ../__para.labeled.letters
 while [[ $dir -le $numdirs ]]
 do
-    mkdir ../__data.labeled.letters/$dir
+    mkdir ../__para.labeled.letters/$dir
     i=0
     while read line; do
-	cp $dir*/$i*.csv ../__data.labeled.letters/$dir/$line\_$dir.csv
+	cp $dir\_*/$i.letter.csv ../__para.labeled.letters/$dir/$line\_$dir.csv
+	echo "copied $dir\_*/$i.letter.csv to ../__para.labeled.letters/$dir/$line\_$dir.csv"
 	((i = i + 1))
+	#sleep .1
     done < ../labels/$dir.txt
     ((dir = dir + 1))
 done
@@ -59,25 +61,25 @@ cd ../../../code/scripts
 printf "labelled: letters and separated\n\n"
 
 # move all the letters into one directory
-rm -rf $path/__data/all-labelled-letters
-mkdir $path/__data/all-labelled-letters
-find $path/__data/__data.labeled.letters -name "*csv" |\
-xargs -I {} cp {} ../../data/__data/all-labelled-letters
+rm -rf $path/__para/all-labelled-letters
+mkdir $path/__para/all-labelled-letters
+find $path/__para/__para.labeled.letters -name "*csv" |\
+xargs -I {} cp {} ../../data/__para/all-labelled-letters
 printf "moved: all letters combined into one directory\n\n"
 
 # separate files into letter folders
-cd $path/__data
+cd $path/__para
 rm -rf all-letters
 mkdir all-letters
 for x in {a..z}
 do
     mkdir all-letters/$x
-    mv all-labelled-letters/$x*csv all-letters/$x 2>/dev/null
+    cp all-labelled-letters/$x*csv all-letters/$x 2>/dev/null
 done
 cd ../../code/scripts
 printf "separated: letters into folders\n\n"
 
 # move the folder outside
 rm -rf $path/all-letters
-cp -r $path/__data/all-letters $path
+cp -r $path/__para/all-letters $path
 find $path/all-letters -type d -empty -exec rmdir {} \;
